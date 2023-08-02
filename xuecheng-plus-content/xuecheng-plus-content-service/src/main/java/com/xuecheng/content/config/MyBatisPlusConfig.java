7390b4768c379dc797bf574e886e41f79c69e940
‘陈水明’ <439752552@qq.com>
2023-08-02 14:37:02 +0800

    content-service模块中新增MyBatisPlusConfig加入分页插件

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/config/MyBatisPlusConfig.java
@@ -0,0 +1,17 @@
+package com.xuecheng.content.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.xuecheng.content.mapper")
+public class MyBatisPlusConfig {
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
