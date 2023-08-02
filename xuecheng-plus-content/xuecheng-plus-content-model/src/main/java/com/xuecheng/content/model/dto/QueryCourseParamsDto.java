595ee4bcdce4df1f457ce7b0ba9ca4f8377e8c25
‘陈水明’ <439752552@qq.com>
2023-08-02 09:36:13 +0800

    添加课程查询条件dto

---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/QueryCourseParamsDto.java
@@ -0,0 +1,23 @@
+package com.xuecheng.content.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * 课程查询条件模型类
+ */
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class QueryCourseParamsDto {
+    //审核状态
+    private String auditStatus;
+    //课程名称
+    private String courseName;
+    //发布状态
+    private String publishStatus;
+
+}
